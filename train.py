6a66eb50d12603ba948a813619ba5049e872829d
alan840706 <58114247+alan840706@users.noreply.github.com>
2020-08-19 18:34:14 +0800

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ imgFolder = "/content/allVideo"
 saveYoloPath = "/content/result"
 classList = { "person":0 }
 
-modelYOLO = "yolov3-tiny"  #yolov3 or yolov3-tiny
+modelYOLO = "yolov2-tiny"  #yolov3 or yolov3-tiny
 testRatio = 0.0
 cfgFolder = "cfg.person"
 cfg_obj_names = "obj.names"
@@ -211,7 +211,7 @@ if(modelYOLO == "yolov3"):
     fileCFG = "yolov3.cfg"
 
 else:
-    fileCFG = "yolov3-tiny.cfg"
+    fileCFG = "yolov2-tiny.cfg"
 
 with open(os.path.join("./makeYOLOv3/cfg",fileCFG)) as file:
     file_content = file.read()
