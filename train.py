1dacfa4df1a30e75d1eda824ebd341acdb704c0b
alan840706 <58114247+alan840706@users.noreply.github.com>
2020-08-21 16:14:46 +0800

    Update train.py

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ saveYoloPath = "/content/result"
 classList = { "person":0 }
 
 modelYOLO = "yolov2-tiny"  #yolov3 or yolov3-tiny
-testRatio = 0.0
+testRatio = 0.2
 cfgFolder = "cfg.person"
 cfg_obj_names = "obj.names"
 cfg_obj_data = "obj.data"
